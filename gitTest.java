471cb0f85595e9c616bdf0db87b2ef0853b65505
jx <975475883@qq.com>
2021-10-02 11:41:35 +0800

    first commit

---
+++ gitTest.java
@@ -0,0 +1,12 @@
+package com.jx;
+
+/**
+ * @author jx
+ * @craete 2021-10-02 11:33
+ */
+public class gitTest implements Comparable{
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+}
