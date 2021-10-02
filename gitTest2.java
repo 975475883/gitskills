58d66b4132ad57920ddab69e5927b800e97b0d5c
jx <975475883@qq.com>
2021-10-02 11:46:04 +0800

    third commit

---
+++ gitTest2.java
@@ -0,0 +1,11 @@
+package com.jx.java4;
+
+import java.io.Serializable;
+
+/**
+ * @author jx
+ * @craete 2021-10-02 11:43
+ */
+public class gitTest2 implements Serializable {
+    public gitTest2(){ };
+}
