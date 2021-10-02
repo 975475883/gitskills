ef4c1ffb21c9339be196affc225f1c6f0eea54e2
jx <975475883@qq.com>
2021-10-02 11:43:09 +0800

    second commit

---
+++ gitTest1.java
@@ -0,0 +1,14 @@
+package com.jx.java4;
+
+import java.util.Comparator;
+
+/**
+ * @author jx
+ * @craete 2021-10-02 11:36
+ */
+public class gitTest1 implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
